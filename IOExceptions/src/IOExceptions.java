63d83e518e9ec4ba68e08181c31921e61b58d7e1
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-04-08 11:49:56 +0200

    Refactor IOExceptions and Main classes for better exception handling

---
+++ IOExceptions/src/IOExceptions.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+
+public class IOExceptions {
+    public static void main(String[] args) throws Exception {
+        try {
+            etaValida(12);
+        } catch (InvalidEtaException ie) {
+            System.out.println("InvalidEtaException " + ie.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("IOException " + ioe.getMessage());
+        }
+
+    }
+
+    public static void etaValida(int eta) throws InvalidEtaException, IOException {
+        // controllo sull'eta
+        if (eta < 18) {
+            throw new InvalidEtaException("devi avere 18 anni"); // invoco la classe
+        } else {
+            System.out.println("Benvenuto a votare");
+        }
+    }
+}
