5cb1d01565247e2ce5255a38a77cde19f0ba0cb3
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-04-09 09:00:31 +0200

    Project cerchio

--- cerchiRandom/src/Cerchio.java
+++ cerchiRandom/src/Cerchio.java
@@ -1,75 +1,43 @@
-
 import java.awt.Color;
 import java.util.Random;
 
 public class Cerchio {
-    private Random r;
+
+    private Random rand;
     private int width, height; // dimensioni della finestra
 
     private int x, y;
     private int d;
     private Color c;
 
-    public Cerchio(int width, int height) {
-        r = new Random();
-        this.width = width;
-        this.height = height;
-
-        Color c = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),128);
-        int d = r.nextInt(25, 175); // dimensione minima
-        int x = r.nextInt(width - d);
-        int y = r.nextInt(height - d);
-
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public int getX() {
         return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
     }
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
-
     public int getD() {
         return d;
-    }
-
-    public void setD(int d) {
-        this.d = d;
     }
 
     public Color getC() {
         return c;
     }
 
-    public void setC(Color c) {
-        this.c = c;
+    public Cerchio(int w, int h) {
+
+        rand = new Random();
+        width = w;
+        height = h;
+
+        // generazione casuale del colore
+        c = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256), 128);
+
+        // coordinate del cerchio
+        d = rand.nextInt(25, 175); // dimensione minima
+        x = rand.nextInt(width - d);
+        y = rand.nextInt(height - d);
     }
-
-
-    
 }
--- cerchiRandom/src/Main.java
+++ cerchiRandom/src/Main.java
@@ -3,26 +3,19 @@ import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // finestra
-        JFrame f = new JFrame("Window");
-        
-        // chiusura finestra
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
-        // pannello
-        Canvas tela = new Canvas();
-        f.add(tela);
-        
-        // risoluzione 
-        f.setSize(600, 400); 
-        
-        // finestra diventa visibile
-        f.setVisible(true); 
-        
-        // centro
-        f.setLocationRelativeTo(null); 
 
+        JFrame f = new JFrame("Cerchi randomizzati");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        int w = 600;
+        int h = 400;
+        f.setSize(w, h);
+
+        Canvas c = new Canvas(w, h);
+        f.add(c);
+
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
     }
 
 }
