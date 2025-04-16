828c68767921532b4290a4b3a79661dd7a723fe3
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-04-16 09:22:23 +0200

    Refactor Dado class to add dice locking functionality

--- yahtzee/src/Dado.java
+++ yahtzee/src/Dado.java
@@ -1,22 +1,43 @@
 import java.util.Random;
 
 public class Dado {
-    
+
     // logica dado
     private Random r;
     private int faccia;
-    
+
+    private boolean bloccato;
+
     public Dado() {
         r = new Random();
         lancia();
+        bloccato = false;
+    }
+
+    public void lancia() {
+        if (!bloccato) {
+            faccia = r.nextInt(1, 7);
+        }
     }
     
-    public void lancia() {
-        faccia = r.nextInt(1, 7);
+    public void blocca() {
+        bloccato = true;
+    }
+    
+    public void sblocca() {
+        bloccato = false;
+    }
+
+    public void invertiStato() {
+        bloccato = !bloccato;
+    }
+
+    public boolean isBloccato() {
+        return bloccato;
     }
     
     public int getFaccia() {
         return faccia;
     }
-    
+
 }
