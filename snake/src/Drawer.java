72bda8c283e880564f8e9489bd1a578cd1d4a6f9
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-05-21 08:10:05 +0200

    Feat: add Menu

--- snake/src/Drawer.java
+++ snake/src/Drawer.java
@@ -1,4 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Drawer {
 
@@ -7,11 +12,31 @@ public class Drawer {
         JFrame frame = new JFrame("Snake");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JMenuBar barra = new JMenuBar();
+        JMenuItem restart = new JMenuItem("Nuova Partita");
+        JMenu about = new JMenu("?");
+        JMenuItem info = new JMenuItem("Snake by Marconi 2025");
+
+        barra.add(restart);
+        about.add(info); 
+        barra.add(about);
+        
+
+        frame.getContentPane().add(barra, BorderLayout.NORTH);
+
+        restart.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                System.out.println("Nuova Partita");
+                Canvas gioco = new Canvas();
+                frame.getContentPane().add(gioco);
+
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+
         frame.setSize(518, 563);
-
-        Canvas c = new Canvas();
-
-        frame.getContentPane().add(c);
 
         frame.setVisible(true);
         frame.setResizable(false);
